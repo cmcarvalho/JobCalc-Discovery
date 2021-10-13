d1071b2023f938339200905456ab3aed5dabc709
Cristiano Carvalho Machado <69367177+cmcarvalho@users.noreply.github.com>
2021-10-13 16:44:15 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const server = express();
+const routes = require("./routes");
 
 // habilitar arquivos statics
 server.use(express.static("public"));
 
-// request, response
-server.get("/", (request, response) => {
-  return response.sendFile(__dirname + "/views/index.html");
-});
-
+// routes
+server.use(routes);
 server.listen(3000, () => console.log("rodando"));
